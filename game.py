fdcc1268729006a30550973af1733086bd7ac80d
stylesuxx <stylesuxx@gmail.com>
2014-06-03 00:55:51 +0200

    Initial commit.

---
+++ game.py
@@ -0,0 +1,19 @@
+"""
+Hold information about a game.
+"""
+class Game:
+    slug = None
+    name = None
+    description = None
+    manufacturer = None
+    year = None
+    cloneof = None
+    state = 'unknown'
+    played = 0
+    time = 0
+
+    def __init__(self):
+        "Constructor"
+
+    def __del__(self):
+        "Destructor"
