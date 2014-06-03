82a9cb98df9126d0d1703e608060a81e3c7f3d93
stylesuxx <stylesuxx@gmail.com>
2014-06-03 22:55:42 +0200

    Added parser for command line arguments: font size, maximum image size, artwork path

--- py_menu.py
+++ py_menu.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python
+import argparse
+
 from menu import *
 
-"Parse arguments with argparse"
-
-game_xml_path = 'game.xml'
-
-"Check if font size was passed"
-
-"Check if max size was passed"
+"Parse command line arguments or display usage"
+parser = argparse.ArgumentParser(description = "Python framebuffer mame menu.")
+parser.add_argument("xmlpath", metavar = "XML_PATH", help = "path to game xml file")
+parser.add_argument("-f", "--fontsize", metavar = "SIZE", help = "set a font size", type = int)
+parser.add_argument("-a", "--artwork", metavar = "PATH", help = "path to artwork directory")
+parser.add_argument("-i", "--imagesize", nargs = 2, metavar = ("WIDTH", "HEIGHT"), help = "maximum image size", type = int)
+args = parser.parse_args()
 
 "Initialize games and menu"
-games = Games(game_xml_path)
+games = Games(args.xmlpath)
 menu = Menu(games)
 
-"""
-The main loop:
-* Draw the menu items
-* Draw the current items image
-* Draw the current items Info
-* Update the display
-* Process keyboard input
-"""
+"Menu settings depending on command line arguments"
+if args.fontsize:
+	menu.set_font_size(args.fontsize)
+if args.imagesize:
+	menu.set_max_image_size((args.imagesize[0], args.imagesize[1]))
+if args.artwork:
+	menu.set_artwork_path(args.artwork)
+
+"Run the main loop until escape is pressed"
 while menu.running:
     menu.draw_items()
     menu.draw_image()
     menu.draw_info()
     menu.update()
+
     menu.check_input()
